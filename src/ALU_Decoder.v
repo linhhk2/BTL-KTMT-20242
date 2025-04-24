047b5360ec37b06e9ea665e56a7e92a99026d104
linhhk2 <90994354+linhhk2@users.noreply.github.com>
2025-04-24 22:14:38 +0700

    Update ALU_Decoder.v

--- src/ALU_Decoder.v
+++ src/ALU_Decoder.v
@@ -1,27 +1,23 @@
-
-module ALU_Decoder(ALUOp,funct3,funct7,op,ALUControl);
-
-    input [1:0]ALUOp;
-    input [2:0]funct3;
-    input [6:0]funct7,op;
-    output [2:0]ALUControl;
-
-    // Method 1 
-    // assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
-    //                     (ALUOp == 2'b01) ? 3'b001 :
-    //                     (ALUOp == 2'b10) ? ((funct3 == 3'b000) ? ((({op[5],funct7[5]} == 2'b00) | ({op[5],funct7[5]} == 2'b01) | ({op[5],funct7[5]} == 2'b10)) ? 3'b000 : 3'b001) : 
-    //                                         (funct3 == 3'b010) ? 3'b101 : 
-    //                                         (funct3 == 3'b110) ? 3'b011 : 
-    //                                         (funct3 == 3'b111) ? 3'b010 : 3'b000) :
-    //                                        3'b000;
-
-    // Method 2
-    assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
-                        (ALUOp == 2'b01) ? 3'b001 :
-                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} == 2'b11)) ? 3'b001 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} != 2'b11)) ? 3'b000 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
-                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
-                                                                  3'b000 ;
-endmodule
+module ALU_Control(        
+    input [2:0] funct3,         
+    input [6:0]funct7,        
+    input [1:0] ALUOp,          
+    output reg [3:0] ALUcontrol_Out 
+);
+always @(*) begin
+  case ({ALUOp, funct7, funct3})
+    12'b10_0000000_000 : ALUcontrol_Out <= 4'b0000;    // ADD 
+    12'b00_0000000_000 : ALUcontrol_Out <= 4'b0000;    // ADD 
+    12'b00_0000000_001 : ALUcontrol_Out <= 4'b0000;    // ADD 
+    12'b00_0000000_010 : ALUcontrol_Out <= 4'b0000;    // ADD 
+    12'b10_0100000_000 : ALUcontrol_Out <= 4'b0001;    // SUB 
+    12'b10_0000000_111 : ALUcontrol_Out <= 4'b0010;    // AND
+    12'b10_0000000_110 : ALUcontrol_Out <= 4'b0011;    // OR
+    12'b10_0000000_100 : ALUcontrol_Out <= 4'b0100;    // XOR
+    12'b10_0000000_001 : ALUcontrol_Out <= 4'b0101;    // SLL
+    12'b10_0000000_101 : ALUcontrol_Out <= 4'b0110;    // SRL
+    12'b10_0100000_101 : ALUcontrol_Out <= 4'b0111;    // SRA
+    12'b10_0000000_010 : ALUcontrol_Out <= 4'b1000;    // SLT
+    default            : ALUcontrol_Out <= 4'b0000; 
+  endcase
+end
